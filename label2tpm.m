4dacb9d3f31102965baaa1e538680b7b46cc1195
Qianqian Fang <fangqq@gmail.com>
2020-02-22 17:31:42 -0500

    add function to convert from TPM to label and back

---
+++ label2tpm.m
@@ -0,0 +1,49 @@
+function seg=label2tpm(vol, names)
+%
+% seg=label2tpm(vol)
+%   or
+% seg=label2tpm(vol, names)
+%
+% converting a multi-lable volume to binary tissue probablistic maps (TPMs) 
+%
+% author: Qianqian Fang (q.fang at neu.edu)
+%
+% input:
+%    vol: a 2-D or 3-D array of integer values.
+%    names (optional): a cell array of strings defining the names of each label,
+%         alternatively, a containers.Map object with label (integer) as
+%         the key and name as the value. The names can be a subset of all
+%         labels.
+%
+% output:
+%    seg: a struct with subfields of 3D or 4D uint8 array; the subfield 
+%         names are defined via the optional names input; the unnamed
+%         labels will be named as 'label_#'. 
+%
+% -- this function is part of brain2mesh toolbox (http://mcx.space/brain2mesh)
+%    License: GPL v2 or later, see LICENSE.txt for details
+%
+
+if(~isnumeric(vol))
+    error('input must be a numerical array');
+end
+
+val=setdiff(sort(unique(vol(:))),0);
+if(length(val)>numel(vol)*(0.1^ndims(vol)))
+    error('please convert the input to labels first');
+end
+
+seg=struct;
+for i=1:length(val)
+    nm=sprintf('label_%d',val(i));
+    if(nargin>1)
+        if(iscell(names))
+            if(i<=length(names))
+                nm=names{i};
+            end
+        elseif(isa(names,'containers.Map') && isKey(names,val(i)))
+            nm=names(val(i));
+        end
+    end
+    seg.(nm)=uint8(vol==val(i));
+end
---
+++ tpm2label.m
@@ -0,0 +1,48 @@
+function [vol, names]=tpm2label(seg, segorder)
+%
+% vol=tpm2label(seg)
+%   or
+% vol=tpm2label(seg, segorder)
+% [vol, names]=tpm2label(seg, segorder)
+%
+% converting tissue probablistic maps (TPMs) to a multi-lable volume
+%
+% author: Qianqian Fang (q.fang at neu.edu)
+%
+% input:
+%    seg: a struct, a cell array, or a 3D or 4D array; if seg is a cell
+%         or a struct, their subfields must be 2D/3D arrays of the same
+%         sizes;
+%    segorder: if seg is a struct, segorder allows one to assign output
+%         labels using customized order instead of the creation order
+%
+% output:
+%    vol: a 2-D or 3-D array of the same type/size of the input arrays. The
+%         label for each voxel is determined by the index to the highest
+%         value in TPM of the same voxel. If a voxel is a background voxel
+%         - i.e. zeros for all TPMs, it stays 0
+%    names: a cell array storing the names of the labels (if input is a
+%         struct), the first string is the name for label 1, and so on
+%
+% -- this function is part of brain2mesh toolbox (http://mcx.space/brain2mesh)
+%    License: GPL v2 or later, see LICENSE.txt for details
+%
+
+mask=seg;
+names={};
+if(isstruct(seg))
+    if(nargin>1)
+        seg=orderfields(seg,segorder);
+    end
+    names=fieldnames(seg);
+    mask=cellfun(@(x) seg.(x), names,'UniformOutput',false);
+end
+if(iscell(mask))
+    mask=cat(ndims(mask{1})+1,mask{:});
+end
+if(~isnumeric(mask))
+    error('input must be a cell/struct array with numeric elements of matching dimensions');
+end
+
+[newmask, vol]=max(mask,[],ndims(mask));
+vol=vol .* (sum(mask,ndims(mask))>0);
