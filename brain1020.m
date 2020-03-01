9c89e03665ae92113893c58ef6a830d0c7bcb348
Qianqian Fang <fangq@nmr.mgh.harvard.edu>
2020-03-01 00:58:13 -0500

    added comments for helper functions, tested on R2013

--- brain1020.m
+++ brain1020.m
@@ -4,12 +4,13 @@ function [landmarks, initpoints, lines]=brain1020(node, elem, initpoints, perc1,
 %   or
 % landmarks=brain1020(node, elem, initpoints)
 % landmarks=brain1020(node, elem, initpoints, 10, 10)
+% landmarks=brain1020(node, elem, initpoints, 10, 10, options)
 %
 % compute 10-20-like scalp landmarks with user-specified density on a head mesh
 %
 % author: Qianqian Fang (q.fang at neu.edu)
 %
-% input:
+% == Input ==
 %    node: full head mesh node list
 %    elem: full head mesh element list- a 3-column array defines face list
 %          for the exterior (scalp) surface; a 4-column array defines the
@@ -25,12 +26,23 @@ function [landmarks, initpoints, lines]=brain1020(node, elem, initpoints, perc1,
 %    perc2:(optional) the percentage of geodesic distance twoards the center 
 %          of the landmarks; this is the 2nd number of the 10-20 or 10-10 or
 %          10-5 systems, which are 20, 10, 5, respectively, default is 20
+%    options: one can add additional 'name',value pairs to the function
+%          call to provide additional control. Supported optional names
+%          include
+%           'display' : [1] or 0, if set to 1, plot landmarks and curves
+%           'baseplane' : [1] or 0, if set to 1, create the reference
+%                   curves along the primary control points (nz,iz,lpa,rpa)
 %
-% output:
+% == Output ==
 %    landmarks: a 3-column array defining the head surface landmark
 %          positions at the requested spacing.
 %
 % 
+% == Dependency ==
+% This function requires a pre-installed Iso2Mesh Toolbox
+%  Download URL: http://github.com/fangq/iso2mesh
+%  Website: http://iso2mesh.sf.net
+%
 % == Reference ==
 % If you use this function in your publication, the authors of this toolbox
 % apprecitate if you can cite the below paper
@@ -64,6 +76,10 @@ end
 
 opt=varargin2struct(varargin{:});
 
+showplot=jsonopt('display',1,opt);
+baseplane=jsonopt('baseplane',1,opt);
+
+
 if(isstruct(initpoints))
     initpoints=orderfields(initpoints,{'nz', 'iz', 'lpa', 'rpa', 'cz'});
     landmarks=initpoints;
@@ -79,32 +95,43 @@ if(size(elem,2)>=4)
     elem=volface(elem(:,1:4));
 end
 
+[node,elem]=removeisolatednode(node,elem);
+
 if(isempty(initpoints) && size(initpoints,1)<5)
     hf=figure;
-    plotmesh(node,elem,'facecolor','b');
-    camlight; lighting phong
-    datacursormode(hf,'on');
+    plotmesh(node,elem);
+    title('Rotate the mesh, select data cursor, click on P1: Nasion');
+    %datacursormode(hf,'on');
+    rotate3d('on');
     set(datacursormode(hf),'UpdateFcn',@myupdatefcn);
 end
 
 if(exist('hf','var'))
-    while(size(get(hf,'userdata'),1)<5)
-        pause(0.1);
+    try
+        while(size(get(hf,'userdata'),1)<5)
+            pause(0.1);
+        end
+    catch
+        error('user aborted');
     end
     datacursormode(hf,'off');
     initpoints=get(hf,'userdata');
     close(hf);
 end
 
+if(showplot)
+    disp(initpoints);
+end
 
 % at this point, initpoints contains {nz, iz, lpa, rpa, cz0}
 
-if(jsonopt('display',1,opt))
+if(showplot)
     figure;
     plotmesh(node,elem,'linestyle','none','facealpha',0.3,'facecolor','b');
     camlight; lighting phong
     hold on;
 end
+
 %% Step b: nz, iz and cz0 to determine saggital reference curve
 nsagg=slicesurf(node, elem, initpoints([1,2,5],:));
 
@@ -122,7 +149,9 @@ initpoints(5,:)=coro(1,:);
 landmarks.cz=coro(1,:);      % using UI 10-10 approach
 landmarks.c0=coro(2:end,:);  % t7, c3, cz, c4, t8
 
-initpoints
+if(showplot)
+    disp(initpoints);
+end
 
 %% Step d/e: subdivide saggital and coronal ref curves
 
@@ -141,7 +170,7 @@ landmarks.s0=sagg;           % fpz, fz, cz, pz, oz
 
 
 %% debug
-if(jsonopt('display',1,opt))
+if(showplot)
     plotmesh(lines.nsagg,'r-','LineWidth',1);
     plotmesh(lines.ncoro,'g-','LineWidth',1);
     plotmesh(lines.nalaxis,'k-','LineWidth',1);
@@ -149,54 +178,81 @@ if(jsonopt('display',1,opt))
     plotmesh(lines.naraxis,'k-','LineWidth',1);
     plotmesh(lines.npraxis,'b-','LineWidth',1);
 
-    plotmesh(landmarks.s0,'ro','MarkerSize',2);
-    plotmesh(landmarks.c0,'go','MarkerSize',2);
-    plotmesh(landmarks.nal,'ko','MarkerSize',2);
-    plotmesh(landmarks.nar,'mo','MarkerSize',2);
-    plotmesh(landmarks.npl,'ko','MarkerSize',2);
-    plotmesh(landmarks.npr,'mo','MarkerSize',2);
+    plotmesh(landmarks.s0,'ro','LineWidth',2);
+    plotmesh(landmarks.c0,'go','LineWidth',2);
+    plotmesh(landmarks.nal,'ko','LineWidth',2);
+    plotmesh(landmarks.nar,'mo','LineWidth',2);
+    plotmesh(landmarks.npl,'ko','LineWidth',2);
+    plotmesh(landmarks.npr,'mo','LineWidth',2);
 end
 
 %% Step j: f8, fz and f7 to determine front coronal cut
 
 idxcz=closestnode(landmarks.s0,landmarks.cz);
 
-for i=1:size(landmarks.nal,1)-1
-    step=perc2/10*25*(1+(perc2<20 && i==size(landmarks.nal,1)-1));
+skipcount=floor(10/perc2);
+
+for i=1:size(landmarks.nal,1)-skipcount
+    step=(perc2*25)*0.1*(1+((perc2<20 + perc2<10) && i==size(landmarks.nal,1)-skipcount));
     [landmarks.(sprintf('cal_%d',i)), leftpart, landmarks.(sprintf('car_%d',i)), rightpart]=slicesurf3(node,elem,landmarks.nal(i,:), landmarks.s0(idxcz-i,:), landmarks.nar(i,:),step);
-    if(jsonopt('display',1,opt))
-        plotmesh(leftpart,'k-','LineWidth',2);
-        plotmesh(rightpart,'k-','LineWidth',2);
-
-        plotmesh(landmarks.(sprintf('cal_%d',i)),'yo','MarkerSize',2);
-        plotmesh(landmarks.(sprintf('car_%d',i)),'co','MarkerSize',2);
-    end
-end
-
-for i=1:size(landmarks.npl,1)-1
-    step=perc2/10*25*(1+(perc2<20 && i==size(landmarks.nal,1)-1));
-    [landmarks.(sprintf('cpl_%d',i)), leftpart, landmarks.(sprintf('cpr_%d',i)), rightpart]=slicesurf3(node,elem,landmarks.npl(i,:), landmarks.s0(idxcz+i,:), landmarks.npr(i,:),step);
-    if(jsonopt('display',1,opt))
+    if(showplot)
         plotmesh(leftpart,'k-','LineWidth',1);
         plotmesh(rightpart,'k-','LineWidth',1);
 
-        plotmesh(landmarks.(sprintf('cpl_%d',i)),'yo','MarkerSize',2);
-        plotmesh(landmarks.(sprintf('cpr_%d',i)),'co','MarkerSize',2);
+        plotmesh(landmarks.(sprintf('cal_%d',i)),'yo','LineWidth',2);
+        plotmesh(landmarks.(sprintf('car_%d',i)),'co','LineWidth',2);
     end
 end
 
+for i=1:size(landmarks.npl,1)-skipcount
+    step=(perc2*25)*0.1*(1+((perc2<20 + perc2<10) && i==size(landmarks.nal,1)-skipcount));
+    [landmarks.(sprintf('cpl_%d',i)), leftpart, landmarks.(sprintf('cpr_%d',i)), rightpart]=slicesurf3(node,elem,landmarks.npl(i,:), landmarks.s0(idxcz+i,:), landmarks.npr(i,:),step);
+    if(showplot)
+        plotmesh(leftpart,'k-','LineWidth',1);
+        plotmesh(rightpart,'k-','LineWidth',1);
+
+        plotmesh(landmarks.(sprintf('cpl_%d',i)),'yo','LineWidth',2);
+        plotmesh(landmarks.(sprintf('cpr_%d',i)),'co','LineWidth',2);
+    end
+end
+
+%% Step f,h,i: fpz, t7 and oz to determine left 10% axial reference curve
+
+if(baseplane && perc2<=10)
+    [landmarks.lal, lines.lalaxis, landmarks.lpl, lines.lplaxis]=slicesurf3(node,elem,landmarks.nz, landmarks.lpa, landmarks.iz, perc2*2);
+    [landmarks.lar, lines.laraxis, landmarks.lpr, lines.lpraxis]=slicesurf3(node,elem,landmarks.nz, landmarks.rpa, landmarks.iz, perc2*2);
+    if(showplot)
+        plotmesh(lines.lalaxis,'k-','LineWidth',1);
+        plotmesh(lines.laraxis,'k-','LineWidth',1);
+        plotmesh(lines.lplaxis,'k-','LineWidth',1);
+        plotmesh(lines.lpraxis,'k-','LineWidth',1);
+
+        plotmesh(landmarks.lal,'yo','LineWidth',2);
+        plotmesh(landmarks.lpl,'co','LineWidth',2);
+        plotmesh(landmarks.lar,'yo','LineWidth',2);
+        plotmesh(landmarks.lpr,'co','LineWidth',2);
+    end
+end
 
 %% helper functions
 % the respond function when there is a data-tip to popup
 
 function txt=myupdatefcn(empt,event_obj)
+pt=get(gcf,'userdata');
 pos = get(event_obj,'Position');
+if(~isempty(pt) && ismember(pos,pt,'rows'))
+    return;
+end
 %idx=  get(event_obj,'DataIndex');
 txt = {['x: ',num2str(pos(1))],...
        ['y: ',num2str(pos(2))],['z: ',num2str(pos(3))]};
 targetup=get(get(event_obj,'Target'),'parent');
+idx=size(pt,1)+2;
+landmarkname={'Nasion','Inion','Left-ear-lobe','Right-ear-lobe','Vertex/Cz','Done'};
+title(sprintf('Rotate the mesh, select data cursor, click on P%d: %s',idx, landmarkname{idx}));
+disp(['Adding landmark ' landmarkname{idx-1} ':' txt]);
 set(targetup,'userdata',struct('pos',pos));
-pt=get(gcf,'userdata');
 if(size(pt,1)<5)
      set(gcf,'userdata',[pt;pos]);
 end
+%rotate3d('on');
--- slicesurf.m
+++ slicesurf.m
@@ -1,4 +1,4 @@
-function [bcutpos, bcutloop]=slicesurf(node, face, varargin)
+function [bcutpos, bcutloop, bcutvalue]=slicesurf(node, face, varargin)
 %
 % [bcutpos, bcutloop]=slicesurf(node, face, varargin)
 %
@@ -12,11 +12,12 @@ function [bcutpos, bcutloop]=slicesurf(node, face, varargin)
 %    face: an N x 3 interger array specifying the surface triangle indices;
 %
 % output:
-%    bcutpos: the nodes ont he intersection curve
+%    bcutpos: the nodes on the intersection curve
 %    bcutloop: the sequential order of the nodes to form a polyline loop;
 %          the last node is assumed to be connected to the first node; an
 %          nan indicates the end of a loop; the intersection may contain
-%          multiple loops.
+%          multiple loops; if only bcutpos is returned, the nodes will be
+%          made in sequential order.
 %
 %
 % -- this function is part of brain2mesh toolbox (http://mcx.space/brain2mesh)
