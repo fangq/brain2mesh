ff0965f3041b660d7fa6dcbd40c0893ca6c35827
Qianqian Fang <fangqq@gmail.com>
2025-02-25 21:08:21 -0500

    [ci] debug octave 5.2 error

--- examples/SPM_example_wholehead.m
+++ examples/SPM_example_wholehead.m
@@ -1,11 +1,11 @@
 clear;
-%% Dependencies: brain2mesh, iso2mesh, and zmat (http://github.com/fangq/zmat)
+%% Dependencies: brain2mesh, iso2mesh, and zmat (https://github.com/NeuroJSON/zmat)
 
 %% Reading in data from c1-c5 SPM segmentations
 
 % the SPM segmentation files are stored in the text-based JNIfTI format
 % (.jnii) as defined in the JNIfTI file specification
-% https://github.com/fangq/jnifti/
+% https://github.com/NeuroJSON/jnifti/
 
 names = {'gm', 'wm', 'csf', 'skull', 'scalp', 'air'};
 for i = 1:5
@@ -39,7 +39,7 @@ view([-0.6 1.5 0.6]);
 figure;
 plotmesh(node, elem(elem(:, 5) == 5, :), 'FaceColor', [1 1 1], 'EdgeAlpha', 0.6); %% wm
 hold on;
-plotmesh(node, elem(elem(:, 5) == 4, :), 'x>78| y<110', 'FaceColor', [0.35 0.35 0.35], 'EdgeAlpha', 0.6); %% pial
+plotmesh(node, elem(elem(:, 5) == 4, :), 'x>78 | y<110', 'FaceColor', [0.35 0.35 0.35], 'EdgeAlpha', 0.6); %% pial
 plotmesh(node, elem(elem(:, 5) == 3, :), 'x>78 | z<135', 'FaceColor', [0.2 0.6 1], 'EdgeAlpha', 0.6); %% csf
 plotmesh(node, elem(elem(:, 5) == 2, :), 'x>90 | z<130', 'FaceColor', [1 1 0.9], 'EdgeAlpha', 0.6); %% bone
 plotmesh(node, elem(elem(:, 5) == 1, :), 'x>100 | z<125', 'FaceColor', [1 0.8 0.7], 'EdgeAlpha', 0.6); %% scalp
